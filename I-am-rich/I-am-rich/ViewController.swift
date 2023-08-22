0d8c2c322e67c166326fb870f37f5654beb8ea6a
primaam <pmaharyono@gmail.com>
2023-08-22 14:50:17 +0700

    revert i am rich proj

---
+++ I-am-rich/I-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-am-rich
+//
+//  Created by prima anugerah maharyono on 22/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
