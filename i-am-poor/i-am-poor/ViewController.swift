d4e8ec8beb31912a7a37095056f8320bdbb70345
primaam <pmaharyono@gmail.com>
2023-08-30 16:21:48 +0700

    first challenge

---
+++ i-am-poor/i-am-poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-am-poor
+//
+//  Created by prima anugerah maharyono on 30/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
