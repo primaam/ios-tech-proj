728a9c952baf73a43d4ac609114fd5628a00ccc2
primaam <pmaharyono@gmail.com>
2023-08-31 16:02:51 +0700

    add Dicee app

---
+++ dicee/dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by prima anugerah maharyono on 31/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
